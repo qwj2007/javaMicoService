46c90d44f45aeee628a3712f03eb5e35d5e0dea1
qwj2007 <qwj2007@163.com>
2020-07-13 16:27:34 +0800

    springcloud 整合skywalking

--- springbootadmin/src/main/java/com/qiancheng/springbootadmin/SpringbootadminApplication.java
+++ springbootadmin/src/main/java/com/qiancheng/springbootadmin/SpringbootadminApplication.java
@@ -2,11 +2,12 @@ package com.qiancheng.springbootadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@Configuration
+
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 
 public class SpringbootadminApplication {
 
